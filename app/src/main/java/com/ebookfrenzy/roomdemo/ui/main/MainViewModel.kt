c64ad18b46e52aece363d4ba122f129605a8fd05
Ian Chai <ianchai@mmu.edu.my>
2021-09-29 16:35:13 +0800

    class MainViewModel(application: Application) : AndroidViewModel(application)

--- app/src/main/java/com/ebookfrenzy/roomdemo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ebookfrenzy/roomdemo/ui/main/MainViewModel.kt
@@ -1,7 +1,19 @@
 package com.ebookfrenzy.roomdemo.ui.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.ebookfrenzy.roomdemo.Product
+import com.ebookfrenzy.roomdemo.ProductRepository
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: ProductRepository = ProductRepository(application)
+    private val allProducts: LiveData<List<Product>>?
+    private val searchResults: MutableLiveData<List<Product>>
+
+    init {
+        allProducts = repository.allProducts
+        searchResults = repository.searchResults
+    }
 }
